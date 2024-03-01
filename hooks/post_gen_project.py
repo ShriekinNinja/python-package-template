e4a6327c12eeb298c0f83ce381b3d2c00a4e23b3
Shriekin’ Ninja <ninja@l-m-group.com>
2024-03-01 11:11:00 +0200

    init scripts

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -65,14 +65,36 @@ def print_futher_instuctions(project_name: str, github: str) -> None:
         github: GitHub username
     """
     message = f"""
-    #############################################################
-    ##  Your project {project_name} is created.                ##
-    ##  Run init script BEFORE opening the project in Pycharm  ##
-    #############################################################
-
+    ####################################################
+    ##  {project_name} is created.                    ##
+    ##  Follow these steps BEFORE opening in Pycharm  ##
+    ####################################################
     $ cd {project_name}
-    $ chmod +x init.sh
-    $ run init.sh or init.bat depending on your OS
+    1) Setup Pyenv and git:
+
+        $ pyenv local 3.12.2
+        $ git init
+
+    2) If you don't have Poetry installed run:
+
+        $ make poetry-download
+
+    3) Initialize poetry and install pre-commit hooks:
+
+        $ poetry env use $(which python)
+        $ make install
+        $ make pre-commit-install
+
+    4) Run codestyle:
+
+        $ make codestyle
+
+    5) Upload initial code to GitHub:
+
+        $ git add .
+        $ git commit -m ":tada: Initial commit"
+        $ git branch -M main
+        $ git remote add origin https://github.com/{github}/{project_name}.git
     """
     print(textwrap.dedent(message))
 
